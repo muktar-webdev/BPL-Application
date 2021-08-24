e0c4af90f5fc7e1ee829da023826ab956b6254ed
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-08-25 01:45:50 +0600

    Install FontAwesome

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Header.css";
+import bpl from "../../images/bpl_logo.jpg";
 
 const Header = () => {
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <div className="topbar">
+      <a href="./home">
+        <img src={bpl} alt="" />
+      </a>
+      <a href="./team">Team</a>
+      <a href="./team">Player</a>
+      <a href="./team">About</a>
+      <a href="./team">Contact</a>
+      <a href="./team">Sign In</a>
+    </div>
+  );
 };
 
 export default Header;
