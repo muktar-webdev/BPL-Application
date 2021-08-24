150a6735f98dfd3f5601a58962791488a9234308
Md.Muktar Ali <muktar15-2298@diu.edu.bd>
2021-08-25 02:14:56 +0600

    Header Component Completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,36 @@
 import React from "react";
 import "./Header.css";
 import bpl from "../../images/bpl_logo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressCard,
+  faArrowCircleDown,
+  faIdCardAlt,
+  faSignInAlt,
+  faUserFriends,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <div className="topbar">
-      <a href="./home">
-        <img src={bpl} alt="" />
+      <a className="home" href="./home">
+        <h2>BPL</h2>
       </a>
-      <a href="./team">Team</a>
-      <a href="./team">Player</a>
-      <a href="./team">About</a>
-      <a href="./team">Contact</a>
-      <a href="./team">Sign In</a>
+      <a href="./login">
+        <FontAwesomeIcon icon={faSignInAlt} /> Sign In
+      </a>
+      <a href="./contact">
+        <FontAwesomeIcon icon={faIdCardAlt} /> Contact
+      </a>
+      <a href="./about">
+        <FontAwesomeIcon icon={faAddressCard} /> About
+      </a>
+      <a href="./player">
+        <FontAwesomeIcon icon={faArrowCircleDown} /> Player
+      </a>
+      <a href="./team">
+        <FontAwesomeIcon icon={faUserFriends} /> Team
+      </a>
     </div>
   );
 };
